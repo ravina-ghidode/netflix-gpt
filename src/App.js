81e2657b2fe9ee311d2d0710c8c156c0702aa70e
ravina-tech <ravina26ghidode@gmail.com>
2024-08-27 21:44:53 +0530

    setup cra and tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Ravina</h1>
-    </div>
-  );
+  return <div className="text-10xl font-bold text-green-800">Ravina</div>;
 }
 
 export default App;
